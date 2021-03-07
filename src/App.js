c77dac6c1dd7159bb8dd2bf6701bdb9d63342097
Behzad <b.soleimani@rismun.com>
2021-03-07 12:45:55 +0330

    change App & Css files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import RightPanel from "./components/right-panel"
+import LeftPanel from "./components/left-panel"
+import AppProvider from "./redux/store"
 function App() {
   return (
+    <AppProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LeftPanel />
+      <RightPanel />
     </div>
+    </AppProvider>
   );
 }
 
